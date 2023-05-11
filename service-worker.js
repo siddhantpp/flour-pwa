99dc2a5d7b43dce4aeb6291bea14033f64889fba
Siddhant Pardeshi <siddhantpp@gmail.com>
2023-05-10 22:34:33 -0400

    [base] Add impl

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+
+
+self.addEventListener('install', function(event) {
+    var offlineRequest = new Request('/offline.html');
+    event.waitUntil(
+        fetch(offlineRequest).then(function(response) {
+            return caches.open('offline').then(function(cache) {
+                return cache.put(offlineRequest, response);
+            });
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    var request = event.request;
+    if (request.method === 'GET') {
+        event.respondWith(
+            fetch(request).catch(function(error) {
+                return caches.open('offline').then(function(cache) {
+                    return cache.match('/offline.html');
+                });
+            })
+        );
+    }
+});
+
